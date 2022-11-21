2db3be348469b3b3445b9a50938c78f6c84736f4
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-11-22 01:15:07 +0800

    [Fix][Lex] Removed unnecessary return fields for responses, added cors configuration, and added catcher for unhandled rejections and uncaught exceptions

--- server/index.js
+++ server/index.js
@@ -10,20 +10,38 @@ const employeeRoutes = require("./src/routes/employee.route");
 const app = express();
 const server = http.createServer(app);
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Authorization,Content-Type",
+    origin: "http://localhost:5173",
+  })
+);
 app.use(helmet());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (_, res) => {
-  res.status(200).send({ error: false, status: 200, message: "Hello World!" });
+  res.status(200).send({ message: "Hello World!" });
 });
 
-app.use("/api/v1/employee", employeeRoutes);
+app.use("/api/v1/employees", employeeRoutes);
 
 app.use("*", (_, res) => {
-  res.status(404).send({ error: true, status: 404, message: "Resource not found!" });
+  res.status(404).send({ message: "Resource not found!" });
+});
+
+process.on("uncaughtException", (error) => {
+  console.log("[UNCAUGHT EXCEPTION]", error);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.log("[UNHANDLED REJECTION - REASON]", reason);
+  console.log("[UNHANDLED REJECTION - PROMISE]", promise);
+  process.exit(1);
 });
 
 const port = process.env["PORT"] || 3000;
--- server/src/controllers/employee.controller.js
+++ server/src/controllers/employee.controller.js
@@ -4,21 +4,17 @@ const Employee = require("../models/employee.model");
 const create = async (req, res) => {
   const { error: validationError } = Employee.validate(req.body);
   if (validationError)
-    res
-      .status(400)
-      .send({
-        error: true,
-        status: 400,
-        message: validationError.details.map((e) => ({ label: e.context.label, message: e.message })),
-      });
+    res.status(400).send({
+      error: validationError.details.map((e) => ({ label: e.context.label, message: e.message })),
+    });
 
   const employee = new Employee(req.body);
   employee.password = await hashPassword(employee.password);
 
   const { error, data } = await employee.save();
-  if (error) res.status(500).send({ error: true, status: 500, message: "Internal Server Error!" });
+  if (error) res.status(500).send({ error: "Internal Server Error!" });
 
-  res.status(200).send({ error: false, status: 200, message: "Employee added successfully!", data });
+  res.status(200).send(data);
 };
 
 module.exports = {
