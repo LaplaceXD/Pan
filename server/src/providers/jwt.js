5aaf4f9629c71170c64d2fa613efb135029b98f7
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-11-23 00:02:45 +0800

    [Feat][Lex] Added logic for invalidating tokens when logging out

--- server/src/providers/jwt.js
+++ server/src/providers/jwt.js
@@ -2,6 +2,9 @@ const config = require("config");
 const { v4: uuid } = require("uuid");
 const jwt = require("jsonwebtoken");
 
+const redis = require("./redis");
+const client = redis.connect();
+
 function sign(payload) {
   const { secret, ...options } = config.get("jwt");
 
@@ -19,12 +22,35 @@ function verify(token) {
   return new Promise((resolve, reject) => {
     jwt.verify(token, secret, options, function (err, payload) {
       if (err) reject(err);
-      resolve(payload);
+
+      // check if it is currently in the redis cache
+      client.get(`jwt_abel:${payload.jti}`, function (err, result) {
+        if (err) reject(err);
+        else if (result) reject("Invalidated token.");
+
+        resolve(payload);
+      });
     });
+  });
+}
+
+function destroy(token) {
+  return new Promise((resolve, reject) => {
+    verify(token)
+      .then(({ jti }) => {
+        const key = `jwt_label:${jti}`;
+
+        client.set(key, true, function (err, _) {
+          if (err) reject(err);
+          resolve(key);
+        });
+      })
+      .catch((err) => console.log(err));
   });
 }
 
 module.exports = {
   sign,
   verify,
+  destroy,
 };
