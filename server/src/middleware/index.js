6006905bcf6d1f83dc39a95e2ab4d37bcc706d59
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-11-23 20:45:11 +0800

    [Feat][Lex] Created validator middleware

--- server/src/middleware/index.js
+++ server/src/middleware/index.js
@@ -1,9 +1,11 @@
 const allow = require("./allow");
 const auth = require("./auth");
 const error = require("./error");
+const validate = require("./validate");
 
 module.exports = {
   allow,
   auth,
   error,
+  validate,
 };
--- server/src/routes/employee.route.js
+++ server/src/routes/employee.route.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
-const { auth, allow } = require("../middleware");
+const { auth, allow, validate } = require("../middleware");
 const { role } = require("../constants/employee");
 
+const Employee = require("../models/employee.model");
 const employeeController = require("../controllers/employee.controller");
 const router = express.Router();
 
-router.post("/", [auth, allow(role.MANAGER)], employeeController.create);
+router.post("/", [auth, allow(role.MANAGER), validate(Employee.validate)], employeeController.create);
 
 module.exports = router;
