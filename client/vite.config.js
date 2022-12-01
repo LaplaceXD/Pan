38b8f0e497e22211273d13a4403ccdc01fcebc26
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-02 04:51:37 +0800

    [Feat][Lex] Removed styled-components, configured css modules, and added clsx

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,21 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+import path from "node:path";
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "./assets"),
+    },
+  },
   plugins: [react()],
+  css: {
+    modules: {
+      scopeBehaviour: "local",
+      localsConvention: "camelCaseOnly",
+      generateScopedName: "[name]__[local]__[hash:base64:5]",
+    },
+  },
 });
