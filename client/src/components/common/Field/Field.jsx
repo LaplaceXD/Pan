b4e4c21e0654cb5539ecadcfd6f87605747fa7b2
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-02 08:15:15 +0800

    [Feat][Lex] Added validation to login form and added error component to field

--- client/src/components/common/Field/Field.jsx
+++ client/src/components/common/Field/Field.jsx
@@ -1,12 +1,13 @@
 import styles from "./Field.module.css";
 
-function Field({ label, id, ...props }) {
+function Field({ label, id, error, ...props }) {
   return (
     <div className={styles.container}>
       <label className={styles.label} htmlFor={id}>
         {label}
       </label>
       <input className={styles.input} {...props} />
+      {error ? <p className={styles.error}>{error}</p> : null}
     </div>
   );
 }
--- client/src/components/pages/Login/Login.jsx
+++ client/src/components/pages/Login/Login.jsx
@@ -1,3 +1,6 @@
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 import banner from "@assets/imgs/login_banner.jpg";
 import logo from "@assets/Logo_128x128.svg";
 import { Button, Field } from "@components/common";
@@ -5,6 +8,20 @@ import { Button, Field } from "@components/common";
 import styles from "./Login.module.css";
 
 function Login() {
+  const { handleSubmit, touched, errors, handleBlur, handleChange, values } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email.").required("Email is required."),
+      password: Yup.string().required("Password is required."),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <main className={styles.container}>
       <img src={banner} alt="Bread." />
@@ -13,9 +30,27 @@ function Login() {
         <img src={logo} alt="Pan logo." />
         <p className={styles.subtitle}>Welcome back to Pan!</p>
 
-        <form className={styles.form} method="POST">
-          <Field label="User ID:" type="text" id="user_id" name="user" />
-          <Field label="Password:" type="password" id="password" name="pass" />
+        <form className={styles.form} method="POST" onSubmit={handleSubmit}>
+          <Field
+            label="Email:"
+            type="text"
+            id="email"
+            name="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email}
+            error={touched.email && errors.email}
+          />
+          <Field
+            label="Password:"
+            type="password"
+            id="password"
+            name="password"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.password}
+            error={touched.password && errors.password}
+          />
 
           <Button label="Login" type="submit" />
         </form>
