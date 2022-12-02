92684f906434f65ca6a16d8834d3d653b6f388b4
Nathan <nathan.arriesgado0g@gmail.com>
2022-12-02 09:33:14 +0800

    [Feat][Netan] Added the view function for order

---
+++ server/src/routes/order.route.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const { auth, permit, validate } = require("../middleware");
+const { role } = require("../constants/employee");
+const { roles } = require("../providers/permissions");
+
+const Order = require("../models/order.model");
+const orderController = require("../controllers/order.controller");
+const router = express.Router();
+
+router.get(
+    "/", 
+    [auth, permit({ allow: [roles(role.MANAGER)] })],
+    orderController.view
+);
+// router.post(
+//     "/",
+//     [auth, permit({ allow: [roles(role.MANAGER)] }), validate(Product.validate)],
+//     productController.create
+// );
+// router.put(
+//     "/:id",
+//     [auth, permit({ allow: [roles(role.MANAGER)] }), validate(Product.validate)],
+//     productController.edit
+// );
+// router.delete(
+//     "/:id",
+//     [auth, permit({ allow: [roles(role.MANAGER)] })],
+//     productController.remove
+// );
+// router.put(
+//     "/:id/status", 
+//     [auth, permit({ allow: [roles(role.MANAGER)] })],
+//     productController.toggleStatus
+// );
+
+
+module.exports = router;
