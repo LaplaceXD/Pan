aea3523158fca978ed0dfdfd373455f6bea03ecf
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-12 23:31:40 +0800

    [Feat][Lex] Linting + Added missing dependencies to nav compoennt

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import { ProtectedRoutes } from "@components/module";
-import Login from "@components/pages/Login";
 import Home from "@components/pages/Home";
+import Login from "@components/pages/Login";
 import Order from "@components/pages/Order";
 import Product from "@components/pages/Product";
 import Supplier from "@components/pages/Supplier";
-import Account from "@components/pages/Account/Account.jsx";
 import { AuthProvider, useAuth } from "@hooks/Auth";
 
 function LoggedIn() {
@@ -24,12 +22,11 @@ function App() {
           {/*<Route path="/" element={<ProtectedRoutes />}>*/}
           {/*  <Route index element={<LoggedIn />} />*/}
           {/*</Route>*/}
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<Home/>} />
-            <Route path="/order" element={<Order/>} />
-            <Route path="/product" element={<Product/>} />
-            <Route path="/supplier" element={<Supplier/>} />
-            <Route path="/account" element={<Account/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/supplier" element={<Supplier />} />
         </Routes>
       </Router>
 
--- client/src/components/layouts/MainLayout.jsx
+++ client/src/components/layouts/MainLayout.jsx
@@ -1,36 +1,53 @@
-import React from "react";
-import {Link} from "react-router-dom";
-import styles from "./MainLayout.module.css";
-import { FiHome, FiTruck, FiUser, FiLogOut } from 'react-icons/fi';
-import { BiNotepad } from 'react-icons/bi';
-import { BsBoxSeam } from 'react-icons/bs';
-import logo from "@assets/Logo_128x128.svg";
+import { Link } from "react-router-dom";
 
-function MainLayout({children}) {
-    return (
-        <div className={styles.layout}>
-            <header>
-                <div className={styles.navLink}>
-                    <div className={styles.logo}>
-                        <img src={logo} alt="Pan logo." />
-                    </div>
-                    <Link to="/"><FiHome className={styles.navIcon}/>Home</Link>
-                    <Link to="/order"><BiNotepad className={styles.navIcon}/>Orders</Link>
-                    <Link to="/product"><BsBoxSeam className={styles.navIcon}/>Products</Link>
-                    <Link to="/supplier"><FiTruck className={styles.navIcon}/>Supplier</Link>
-                </div>
-                <div className={styles.navLink}>
-                    <Link to="/account"><FiUser className={styles.navIcon}/>Account</Link>
-                    <Link to="/login"><FiLogOut className={styles.navIcon}/>Logout</Link>
-                </div>
-            </header>
-            <main>
-                <div>
-                    {children}
-                </div>
-            </main>
+import { BiNotepad } from "react-icons/bi";
+import { BsBoxSeam } from "react-icons/bs";
+import { FiHome, FiLogOut, FiTruck, FiUser } from "react-icons/fi";
+
+import logo from "@assets/Logo_128x128.svg";
+import styles from "./MainLayout.module.css";
+
+function MainLayout({ children }) {
+  return (
+    <div className={styles.layout}>
+      <header>
+        <div className={styles.navLink}>
+          <div className={styles.logo}>
+            <img src={logo} alt="Pan logo." />
+          </div>
+          <Link to="/">
+            <FiHome className={styles.navIcon} />
+            Home
+          </Link>
+          <Link to="/order">
+            <BiNotepad className={styles.navIcon} />
+            Orders
+          </Link>
+          <Link to="/product">
+            <BsBoxSeam className={styles.navIcon} />
+            Products
+          </Link>
+          <Link to="/supplier">
+            <FiTruck className={styles.navIcon} />
+            Supplier
+          </Link>
         </div>
-    )
+        <div className={styles.navLink}>
+          <Link to="/account">
+            <FiUser className={styles.navIcon} />
+            Account
+          </Link>
+          <Link to="/login">
+            <FiLogOut className={styles.navIcon} />
+            Logout
+          </Link>
+        </div>
+      </header>
+      <main>
+        <div>{children}</div>
+      </main>
+    </div>
+  );
 }
 
-export default MainLayout
+export default MainLayout;
