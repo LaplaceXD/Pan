070baaad36b8660772bbac4f202e45095d4cffac
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-13 16:18:49 +0800

    [Feat][Lex] Created User component

--- client/src/components/module/index.js
+++ client/src/components/module/index.js
@@ -1,3 +1,4 @@
 export { default as LoginForm } from "./LoginForm";
 export { default as Nav } from "./Nav";
 export { default as ProtectedRoutes } from "./ProtectedRoutes";
+export { default as User } from "./User";
