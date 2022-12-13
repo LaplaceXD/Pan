d49a075ac141036d89ebe4b3f0228c56d4f55546
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-13 22:20:23 +0800

    [Feat][Lex] Created options component

--- client/src/components/common/index.js
+++ client/src/components/common/index.js
@@ -1,4 +1,5 @@
 export { default as BoxImage } from "./BoxImage";
 export { default as Button } from "./Button";
 export { default as Field } from "./Field";
+export { default as Options } from "./Options";
 export { default as SearchBar } from "./SearchBar";
