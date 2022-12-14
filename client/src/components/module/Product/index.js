35e983cb256a5a14e19e80c54408cc24b94efcb9
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-14 21:20:06 +0800

    [Fix][Lex] Removed unnecessary components for product

--- client/src/components/module/Product/index.js
+++ client/src/components/module/Product/index.js
@@ -1,11 +1,7 @@
 import Card from "./ProductCard";
-import Grid from "./ProductGrid";
 import Item from "./ProductItem";
-import List from "./ProductList";
 
 export default {
   Card,
-  Grid,
   Item,
-  List,
 };
