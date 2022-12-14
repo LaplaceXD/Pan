c90ccf61301faf4b2d9410b9e3720a5e11deeec3
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-14 23:12:21 +0800

    [Feat][Lex] Added search and options filter functionality

--- client/src/components/pages/Product/Product.jsx
+++ client/src/components/pages/Product/Product.jsx
@@ -1,41 +1,58 @@
 import empImg from "@assets/imgs/emp-img.jpg";
 import { List, Options, SearchBar } from "@components/common";
 import { Product as Prod, UserBanner } from "@components/module";
+import useFilter from "@hooks/filter";
 import useQuery from "@hooks/query";
 import { getAllProducts } from "@services/product";
 
 import styles from "./Product.module.css";
 
+const categoryFilter = ({ category_id }, category) => category_id === category;
+const searchFilter = ({ name, description }, search) => {
+  const searchLower = search.toLowerCase();
+  const nameMatch = name.toLowerCase().includes(searchLower);
+  const descMatch = description.toLowerCase().includes(searchLower);
+
+  return nameMatch || descMatch;
+};
+
 function Product() {
   const { data: products } = useQuery(getAllProducts);
+  const { data, filter, setFilter } = useFilter(products, {
+    search: { value: "", filter: searchFilter },
+    category: { value: 0, filter: categoryFilter },
+  });
 
   return (
     <main className={styles.container}>
       <header className={styles.header}>
         <h1 className={styles.title}>Products Available</h1>
-        <SearchBar className={styles.search} />
-
+        <SearchBar
+          className={styles.search}
+          value={filter.search}
+          onSearch={(e) => setFilter({ search: e.currentTarget.value })}
+        />
         <UserBanner imgSize={56} />
       </header>
 
       <Options
         className={styles.options}
         options={[
-          { label: "All", value: 1 },
-          { label: "Bread", value: 2 },
-          { label: "Cake", value: 3 },
-          { label: "Donuts", value: 4 },
-          { label: "Cookies", value: 5 },
-          { label: "Drinks", value: 6 },
+          { label: "All", value: 0 },
+          { label: "Bread", value: 1 },
+          { label: "Cake", value: 2 },
+          { label: "Donuts", value: 3 },
+          { label: "Cookies", value: 4 },
+          { label: "Drinks", value: 5 },
         ]}
-        value={1}
-        onChange={(value) => console.log(value)}
+        value={filter.category}
+        onChange={(value) => setFilter({ category: value })}
       />
 
       <List
         column
         className={styles.productList}
-        items={products}
+        items={data}
         itemKey={(product) => product.product_id}
         RenderComponent={({ name, description, unit_price }) => (
           <Prod.Item
