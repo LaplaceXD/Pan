2a4ab118abf3dc2d9c9660781a0a526f6b4247af
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-14 18:16:21 +0800

    [Feat][Lex] Added timeout toaster message

--- client/src/hooks/query.js
+++ client/src/hooks/query.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 import { useAuth } from "@hooks/auth";
-import { useNavigate } from "react-router-dom";
 
 function useQuery(query, checkAuth = true) {
   const auth = useAuth();
@@ -12,6 +13,7 @@ function useQuery(query, checkAuth = true) {
   function redirectIfUnauthorized(status) {
     if (status === 401) {
       auth.logout();
+      toast.error("Session timeout.");
       navigate("/login");
     }
   }
