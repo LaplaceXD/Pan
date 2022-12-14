fb7bb76ad4e0e0135e7874dedfb46fd3575ec179
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-14 18:34:10 +0800

    [Fix][Lex] Fixed boxed image styling issue with images not occupying the full size of the box

--- client/src/components/common/BoxImage/BoxImage.jsx
+++ client/src/components/common/BoxImage/BoxImage.jsx
@@ -2,10 +2,10 @@ import { clsx } from "clsx";
 
 import styles from "./BoxImage.module.css";
 
-function BoxImage({ alt, size, src, className, ...props }) {
+function BoxImage({ alt, size, src, className, style, ...props }) {
   return (
-    <div className={clsx(styles.box, className)} {...props}>
-      <img src={src} alt={alt} style={{ width: size, height: size }} />
+    <div className={clsx(styles.box, className)} style={{ width: size, height: size }} {...props}>
+      <img src={src} alt={alt} />
     </div>
   );
 }
