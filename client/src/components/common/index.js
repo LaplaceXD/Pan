b57958b5aa4942a7d702e990c73baf99939521e0
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-15 15:12:06 +0800

    [Feat][Lex] Extracted the modal component

--- client/src/components/common/index.js
+++ client/src/components/common/index.js
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button";
 export { default as Field } from "./Field";
+export { default as Modal } from "./Modal";
 export { default as Portal } from "./Portal";
