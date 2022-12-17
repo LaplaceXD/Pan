e1e6c582a202965047253212fb12c1efe7795f20
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-17 19:36:34 +0800

    [Feat][Lex] Refactored Header in products

--- client/src/components/pages/Product/Product.jsx
+++ client/src/components/pages/Product/Product.jsx
@@ -1,5 +1,5 @@
 import empImg from "@assets/imgs/emp-img.jpg";
-import { List, Options, SearchBar } from "@components/common";
+import { Header, List, Options, SearchBar } from "@components/common";
 import { Product as Prod, UserBanner } from "@components/module";
 import useFilter from "@hooks/filter";
 import useQuery from "@hooks/query";
@@ -25,15 +25,14 @@ function Product() {
 
   return (
     <main className={styles.container}>
-      <header className={styles.header}>
-        <h1 className={styles.title}>Products Available</h1>
+      <Header title="Products Available" className={styles.header}>
         <SearchBar
           className={styles.search}
           value={filter.search}
           onSearch={(e) => setFilter({ search: e.currentTarget.value })}
         />
         <UserBanner imgSize={56} />
-      </header>
+      </Header>
 
       <Options
         className={styles.options}
