bd17c9ed4d15f8b4bca90ad2f2dbd57da0518758
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-17 19:32:58 +0800

    [Feat][Lex] Added missing await to getAllProducts

--- client/src/services/product.js
+++ client/src/services/product.js
@@ -1,5 +1,5 @@
 import auth from "@utils/auth";
 
 export async function getAllProducts({ signal }) {
-  return auth.get("/products", { signal });
+  return await auth.get("/products", { signal });
 }
