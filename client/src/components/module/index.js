affd8a80c6cd22201546749720c6b3dee4dbfa15
Jonh Alexis <67273056+LaplaceXD@users.noreply.github.com>
2022-12-17 20:28:20 +0800

    [Feat][Lex] Added product page functionalties (#23)

--- client/src/components/module/index.js
+++ client/src/components/module/index.js
@@ -1,3 +1,4 @@
+export { default as Category } from "./Category";
 export { default as LoginForm } from "./LoginForm";
 export { default as Modal } from "./Modal";
 export { default as Nav } from "./Nav";
