3c5183619c6c578ff627f70c4f158d5903504e15
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-18 23:16:32 +0800

    [Feat][Lex] Made the CartItemModal value to be dynamic

--- client/src/components/module/Modal/CartItemModal/CartItemModal.jsx
+++ client/src/components/module/Modal/CartItemModal/CartItemModal.jsx
@@ -1,12 +1,12 @@
 import NumberInputModal from "../NumberInputModal";
 
-function CartItemModal({ open, max, onClose, onSubmit }) {
+function CartItemModal({ open, max, value, onClose, onSubmit }) {
   return (
     <NumberInputModal
       name="quantity"
       min={1}
       max={max}
-      initialValue={1}
+      initialValue={value}
       buttonLabel="Add to Cart"
       onSubmit={onSubmit}
       onClose={onClose}
