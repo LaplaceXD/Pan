3d9b1542212212b9248afdc9f8d301d30720959b
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-19 22:53:26 +0800

    [Fix][Lex] Fixed passed parameters to logout, and annotated implementation

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -1,10 +1,14 @@
 import api from "@utils/api";
 import auth from "@utils/auth";
+import token from "@utils/token";
 
 export async function loginEmployee({ email, password }) {
   return await api.post("/auth/login", { email, password });
 }
 
-export async function logoutEmployee(tokens) {
-  return await auth.post("/auth/logout", tokens);
+export async function logoutEmployee() {
+  // The function token.pair.get is passed here to ensure that
+  // if the user needs its session revalidated when logging out
+  // logout would be able to receive the latest tokens on revalidate
+  return await auth.post("/auth/logout", token.pair.get);
 }
