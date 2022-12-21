5d4e870f342ff3771b0786741dfa508e069e31b8
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-21 14:58:23 +0800

    [Feat][Lex] Added ProductForm component

---
+++ client/src/components/module/Product/ProductForm/ProductForm.jsx
@@ -0,0 +1,79 @@
+import { BoxImage, Button, Field } from "@components/common";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+import styles from "./ProductForm.module.css";
+
+function ProductForm({ img, name = "", category = "", description = "", price = 0, onCancel, onSubmit }) {
+  const formik = useFormik({
+    initialValues: { name, category, description, price },
+    validateYupSchema: Yup.object({
+      name: Yup.string().label("Product Name").required(),
+      category: Yup.string().label("Category").required(),
+      description: Yup.string().label("Description").required(),
+      price: Yup.number().label("Unit Price").required(),
+    }),
+    onSubmit: (values) => {
+      onSubmit();
+    },
+  });
+
+  return (
+    <form method="POST" className={styles.container} onSubmit={formik.handleSubmit}>
+      <h3 className={styles.title}>Product Details</h3>
+      <BoxImage className={styles.img} src={img} alt={`${name}'s image.`} size={256} />
+      <Field
+        type="text"
+        label="Product Name"
+        id="name"
+        name="name"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.name}
+        error={formik.touched.name && formik.errors.name}
+      />
+      <Field
+        type="text"
+        label="Category"
+        id="category"
+        name="category"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.category}
+        error={formik.touched.category && formik.errors.category}
+      />
+      <Field
+        type="textarea"
+        label="Description"
+        id="description"
+        className={styles.description}
+        error={formik.touched.description && formik.errors.description}
+      >
+        <textarea
+          id="description"
+          name="description"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.description}
+        />
+      </Field>
+      <Field
+        type="text"
+        label="Unit Price"
+        id="price"
+        name="price"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.price}
+        error={formik.touched.price && formik.errors.price}
+      />
+
+      <div className={styles.buttons}>
+        <Button type="button" label="Cancel" secondary onClick={onCancel} />
+        <Button label="Save" />
+      </div>
+    </form>
+  );
+}
+
+export default ProductForm;
