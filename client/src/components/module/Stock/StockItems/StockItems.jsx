2ddf548ceb291209613467cca26427cc305af073
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-23 00:31:33 +0800

    [Feat][Lex] Added StockItems component

---
+++ client/src/components/module/Stock/StockItems/StockItems.jsx
@@ -0,0 +1,41 @@
+import { List } from "@components/common";
+import StockItem from "../StockItem";
+import styles from "./StockItems.module.css";
+
+function StockItems({ items = [], onItemEdit, onItemDelete, showSupplier = false, showProduct = false }) {
+  return (
+    <List
+      column
+      items={items}
+      className={styles.list}
+      itemKey={(stock) => stock.stock_id}
+      RenderComponent={({
+        stock_id,
+        unit,
+        notes,
+        unit_price,
+        product_id,
+        supplier_id,
+        date_supplied,
+        quantity,
+      }) => (
+        <StockItem
+          unit={unit}
+          quantity={quantity}
+          notes={notes}
+          product={product_id}
+          supplier={supplier_id}
+          dateSupplied={date_supplied}
+          price={unit_price}
+          onEdit={() => onItemEdit(stock_id)}
+          onDelete={() => onItemDelete(stock_id)}
+          showNotes
+          showSupplier={showSupplier}
+          showProduct={showProduct}
+        />
+      )}
+    />
+  );
+}
+
+export default StockItems;
