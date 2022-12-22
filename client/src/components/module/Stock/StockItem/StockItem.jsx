0a22df5bf9440a857bcf273664b2ec059176aa25
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-23 00:31:20 +0800

    [Feat][Lex] Added StockItem component

---
+++ client/src/components/module/Stock/StockItem/StockItem.jsx
@@ -0,0 +1,65 @@
+import { BsCalendarDate, BsTrash, BsTruck } from "react-icons/bs";
+import { FaEdit } from "react-icons/fa";
+import { MdProductionQuantityLimits } from "react-icons/md";
+
+import format from "@utils/format";
+import styles from "./StockItem.module.css";
+
+function StockItem({
+  quantity = 0,
+  unit = "unit",
+  price = 0,
+  supplier = "supplier",
+  dateSupplied = new Date(),
+  notes = "",
+  onEdit,
+  onDelete,
+  showSupplier = false,
+  showProduct = false,
+}) {
+  return (
+    <li className={styles.container}>
+      <article className={styles.content}>
+        <h4 className={styles.header}>
+          {quantity} {unit} <span className={styles.price}>@ {price} each</span>
+        </h4>
+        {showProduct ? (
+          <p>
+            <MdProductionQuantityLimits /> {product_id}
+          </p>
+        ) : null}
+        {showSupplier ? (
+          <p>
+            <BsTruck /> {supplier}
+          </p>
+        ) : null}
+        <p>
+          <BsCalendarDate /> {format.datetime(dateSupplied)}
+        </p>
+      </article>
+      <div className={styles.buttons}>
+        <button
+          type="button"
+          onClick={(e) => {
+            e.stopPropagation();
+            onEdit();
+          }}
+        >
+          <FaEdit size={24} />
+        </button>
+        <button
+          type="button"
+          onClick={(e) => {
+            e.stopPropagation();
+            onDelete();
+          }}
+        >
+          <BsTrash size={24} />
+        </button>
+      </div>
+      <p className={styles.notes}>{notes}</p>
+    </li>
+  );
+}
+
+export default StockItem;
