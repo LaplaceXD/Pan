40ab96a3f45b98644123bf04e286fb895e319052
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-25 05:04:22 +0800

    [Fix][Lex] Passed onDownload props on ReportItem and ReportCart to ReportForm

--- client/src/components/module/Report/ReportCard/ReportCard.jsx
+++ client/src/components/module/Report/ReportCard/ReportCard.jsx
@@ -3,13 +3,13 @@ import banner from "@assets/imgs/login_banner.jpg";
 import ReportForm from "../ReportForm";
 import styles from "./ReportCard.module.css";
 
-function ReportCard({ title }) {
+function ReportCard({ title, onDownload }) {
   return (
     <div className={styles.container}>
       <img src={banner} alt="Bread." className={styles.img} />
       <div className={styles.content}>
         <h1 className={styles.title}>{title}</h1>
-        <ReportForm className={styles.form} />
+        <ReportForm className={styles.form} onDownload={onDownload} />
       </div>
     </div>
   );
--- client/src/components/module/Report/ReportItem/ReportItem.jsx
+++ client/src/components/module/Report/ReportItem/ReportItem.jsx
@@ -3,12 +3,12 @@ import { clsx } from "clsx";
 import ReportForm from "../ReportForm";
 import styles from "./ReportItem.module.css";
 
-function ReportItem({ title, className }) {
+function ReportItem({ title, className, onDownload }) {
   return (
     <div className={clsx(styles.container, className)}>
       <h1 className={styles.title}>{title}</h1>
       <div className={styles.footer}>
-        <ReportForm className={styles.form} />
+        <ReportForm className={styles.form} onDownload={onDownload} />
       </div>
     </div>
   );
