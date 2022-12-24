8ac12afb6287ee901399b17b42a0e7db110be7d2
cham0m1le <66218784+cham0m1le@users.noreply.github.com>
2022-12-24 13:31:32 +0800

    [Feat][Erwin] Reports module created (#50)
    * [Erwin][Feat]Finished all requirements for reports.
    
    * [Erwin][Feat]Finished all requirements for reports.
    
    * [Feat][Lex] Linting + created XLSX provider and fixed queries for the reports
    
    * [Fix][Lex] Uncommented the auth and permission middlewares in report routes
    
    Co-authored-by: Jonh Alexis Buot <ja.buotph@gmail.com>

---
+++ server/src/controllers/report.controller.js
@@ -0,0 +1,55 @@
+const { xlsx } = require("../providers");
+const Report = require("../models/report.model");
+
+function getFileName(fileName, startDate, endDate) {
+  let datePart = "";
+  if (startDate || endDate) datePart = " [" + [startDate, endDate].filter(Boolean).join("-") + "]";
+
+  return `${fileName}${datePart}`;
+}
+
+const salesReport = async (req, res) => {
+  const { start_date, end_date } = req.query;
+
+  const data = await Report.productReport(start_date, end_date);
+  const fileName = getFileName("Sales Report", start_date, end_date);
+  const report = xlsx.generateExcelReport(data);
+
+  res.writeHead(200, {
+    "Content-Type": "application/octet-stream",
+    "Content-disposition": `attachment; filename=${fileName}.xlsx`,
+  });
+  res.end(report);
+};
+const employeeReport = async (req, res) => {
+  const { start_date, end_date } = req.query;
+
+  const data = await Report.employeeReport(start_date, end_date);
+  const fileName = getFileName("Employee Report", start_date, end_date);
+  const report = xlsx.generateExcelReport(data);
+
+  res.writeHead(200, {
+    "Content-Type": "application/octet-stream",
+    "Content-disposition": `attachment; filename=${fileName}.xlsx`,
+  });
+  res.end(report);
+};
+const inventoryReport = async (req, res) => {
+  const { start_date, end_date } = req.query;
+
+  const data = await Report.inventoryReport(start_date, end_date);
+  const fileName = getFileName("Inventory Report", start_date, end_date);
+  const report = xlsx.generateExcelReport(data);
+
+  res.writeHead(200, {
+    "Content-Type": "application/octet-stream",
+    "Content-disposition": `attachment; filename=${fileName}.xlsx`,
+  });
+  res.end(report);
+};
+
+module.exports = {
+  salesReport,
+  employeeReport,
+  inventoryReport,
+};
---
+++ server/src/routes/report.route.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const { auth, permit } = require("../middleware");
+const { role } = require("../constants/employee");
+const { roles } = require("../providers/permissions");
+
+const reportController = require("../controllers/report.controller");
+const router = express.Router();
+
+router.get("/sales", [auth, permit({ allow: [roles(role.MANAGER)] })], reportController.salesReport);
+
+router.get("/employee", [auth, permit({ allow: [roles(role.MANAGER)] })], reportController.employeeReport);
+
+router.get(
+  "/inventory",
+  [auth, permit({ allow: [roles(role.MANAGER)] })],
+  reportController.inventoryReport
+);
+
+module.exports = router;
