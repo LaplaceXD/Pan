5bc92c6cb0d6896dc4f24a363275f02046868bc5
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-25 23:40:08 +0800

    [Feat][Lex] Made the Employee Items clickable and added search functionality

--- client/src/components/pages/Employee/Employee.jsx
+++ client/src/components/pages/Employee/Employee.jsx
@@ -1,45 +1,69 @@
-import empImg from "@assets/imgs/emp-img.jpg";
-import styles from "./Employee.module.css";
+import { useState } from "react";
 
-import { Button, Header, List, SearchBar } from "@components/common";
+import empImg from "@assets/imgs/emp-img.jpg";
+import { Header, List, SearchBar } from "@components/common";
 import { Employee as EmployeeModule } from "@components/module";
 import { PreviewLayout } from "@components/template";
+import { useFilter } from "@hooks";
 import { useEmployees } from "@hooks/services/employee";
+import format from "@utils/format";
+
+import styles from "./Employee.module.css";
+import EmployeePreview from "./EmployeePreview";
+
+const search = {
+  value: "",
+  filter({ employee_id, fullName, contact_no, email }, search) {
+    const searchLower = search.toLowerCase();
+    const idMatched = format.id(employee_id).toLowerCase().includes(searchLower);
+    const fullNameMatched = fullName.toLowerCase().includes(searchLower);
+    const contactMatched = contact_no.toLowerCase().includes(searchLower);
+    const emailMatched = email.toLowerCase().includes(searchLower);
+
+    return idMatched || fullNameMatched || contactMatched || emailMatched;
+  },
+};
 
 function Employee() {
+  const [employeeId, setEmployeeId] = useState(null);
   const {
     payload: { data },
   } = useEmployees();
+
   const employees = data?.map(({ first_name, last_name, ...values }) => ({
     fullName: `${first_name} ${last_name}`,
     ...values,
   }));
 
-  const EmployeePreview = (
-    <>
-      <EmployeeModule.Preview className={styles.employeePreview}></EmployeeModule.Preview>
-      <Button label="Add new Employee" />
-    </>
-  );
+  const { filter, data: filteredData } = useFilter(employees, { search });
 
   return (
-    <PreviewLayout PreviewComponent={EmployeePreview} className={styles.container}>
+    <PreviewLayout
+      PreviewComponent={<EmployeePreview employeeId={employeeId} />}
+      className={styles.container}
+    >
       <Header title="Employee List" className={styles.header}>
-        <SearchBar className={styles.search} />
+        <SearchBar
+          className={styles.search}
+          onSearch={(e) => filter.handleSearch(e.currentTarget.value)}
+          value={filter.search}
+        />
       </Header>
 
       <List
-        className={styles.employeeList}
+        className={styles.list}
         column
-        items={employees}
+        items={filteredData}
         itemKey={(employee) => employee.employee_id}
-        RenderComponent={(employee) => (
+        RenderComponent={({ employee_id, fullName, contact_no, date_employed }) => (
           <EmployeeModule.Item
             img={empImg}
-            id={employee.employee_id}
-            name={employee.fullName}
-            contact_no={employee.contact_no}
-            date={employee.date_employed}
+            id={employee_id}
+            name={fullName}
+            contactNumber={contact_no}
+            date={date_employed}
+            onClick={() => setEmployeeId(employeeId === employee_id ? null : employee_id)}
+            isSelected={employeeId === employee_id}
           />
         )}
       />
