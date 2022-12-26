b30c87ed037d2fdab710249c6d7208b123d50181
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-26 21:23:48 +0800

    [Feat][Lex] Added downloading indicator for buttons in item and fixed styling for empty grid

--- client/src/components/module/Report/ReportItem/ReportItem.jsx
+++ client/src/components/module/Report/ReportItem/ReportItem.jsx
@@ -1,14 +1,26 @@
 import { clsx } from "clsx";
+import { useState } from "react";
 
 import { Button } from "@components/common";
 import styles from "./ReportItem.module.css";
 
 function ReportItem({ title, className, onDownload }) {
+  const [isDownloading, setIsDownloading] = useState(false);
+
   return (
     <div className={clsx(styles.container, className)}>
       <h1 className={styles.title}>{title}</h1>
       <div className={styles.footer}>
-        <Button type="button" label="Download Report" onClick={onDownload} />
+        <Button
+          type="button"
+          label="Download Report"
+          onClick={async () => {
+            setIsDownloading(true);
+            await onDownload();
+            setIsDownloading(false);
+          }}
+          disabled={isDownloading}
+        />
       </div>
     </div>
   );
