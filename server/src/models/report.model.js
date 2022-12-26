f1430459e6737785602ae6c1567ad7f3cb1ce626
Jonh Alexis Buot <ja.buotph@gmail.com>
2022-12-26 15:04:33 +0800

    [Fix][Lex] Fixed inventory report

--- server/src/models/report.model.js
+++ server/src/models/report.model.js
@@ -71,32 +71,126 @@ class Report {
   }
 
   static async getInventoryReportData(startDate, endDate) {
-    let content = null;
+    const content = await Report.retrieveInventory({ startDate, endDate });
+
+    return [
+      {
+        sheet: "Monthly Inventory",
+        columns: [
+          { label: "Product	Name", value: "product_name" },
+          { label: "Beginning Inventory (Qty)", value: "beginning_inventory" },
+          {
+            label: "Beginning Inventory (Avg Price)",
+            value: "avg_price_of_beginning_inventory",
+            format: "₱###,###,###.##",
+          },
+          { label: "Purchases (Qty)", value: "purchases" },
+          {
+            label: "Purchases (Avg Price)",
+            value: "avg_price_of_purchases",
+            format: "₱###,###,###.##",
+          },
+          { label: "Goods Sold (Qty)", value: "number_of_goods_sold" },
+          {
+            label: "Goods Sold (Avg Price)",
+            value: "avg_price_of_goods_sold",
+            format: "₱###,###,###.##",
+          },
+          { label: "Ending Inventory (Qty)", value: "ending_inventory" },
+          {
+            label: "Ending Inventory (Avg Price)",
+            value: "avg_price_of_ending_inventory",
+            format: "₱###,###,###.##",
+          },
+          { label: "Cost of Goods Sold", value: "cost_of_goods_sold", format: "₱###,###,###.00" },
+          { label: "Total Sold", value: "total_sold", format: "₱###,###,###.00" },
+          { label: "Gross Profit", value: "gross_profit", format: "₱###,###,###.00" },
+        ],
+        content,
+      },
+    ];
+  }
+
+  static async retrieveInventory({ startDate, endDate }) {
+    let retVal = null;
 
     try {
       const conn = await db.connect();
       const [data] = await conn.execute(
-        `SELECT 
-          p.name AS product_name, 
-          s.times_stocked, 
-          SUM(ol.quantity) AS quantity_used, 
-          s.total_stock - SUM(ol.quantity) AS quantity_unused, 
-          SUM(ol.quantity * s.average_price) AS cost_of_goods_sold,
-          SUM(ol.quantity * ol.selling_price) AS gross_profit,
-          SUM(ol.quantity * ol.selling_price) - s.total_price AS net_profit 
+        `SELECT
+          p.name AS product_name,
+          
+          -- Beginning Inventory, which is the ending inventory of the previous month
+          COALESCE(prev_inv.ending_inventory, 0) AS beginning_inventory,
+          CAST(ROUND(COALESCE(prev_inv.avg_price_of_ending_inventory, 0), 2) AS DECIMAL(10,2)) AS avg_price_of_beginning_inventory,
+          
+          -- Stock purchased for this month
+          COALESCE(s.purchases, 0) AS purchases,
+          CAST(ROUND(COALESCE(s.avg_price_of_purchases, 0), 2) AS DECIMAL(10,2)) AS avg_price_of_purchases,
+          
+          -- Number of Goods Sold
+          COALESCE(ol.number_of_goods_sold, 0) AS number_of_goods_sold,
+          CAST(ROUND(COALESCE(ol.avg_price_of_goods_sold, 0), 2) AS DECIMAL(10,2)) AS avg_price_of_goods_sold,
+          
+          -- Ending inventory which is beginning inventory + purchased stock this month - number of goods sold
+          COALESCE(prev_inv.ending_inventory, 0) + COALESCE(purchases, 0) - COALESCE(number_of_goods_sold, 0) AS ending_inventory,
+          CAST(ROUND((COALESCE(prev_inv.avg_price_of_ending_inventory, 0) + COALESCE(avg_price_of_purchases, 0)) / 2, 2) AS DECIMAL(10,2)) AS avg_price_of_ending_inventory,
+          
+          -- Cost of Goods Sold = number of goods sold times avg price of ending inventory
+          CAST(ROUND(COALESCE(number_of_goods_sold, 0) * COALESCE(avg_price_of_ending_inventory, 0), 2) AS DECIMAL(15,2)) AS cost_of_goods_sold,
+          
+          -- Total sold = total revenue from the product
+          CAST(ROUND(COALESCE(ol.total_sold, 0), 2) AS DECIMAL(15,2)) AS total_sold,
+
+          -- Gross Profit = Total Sold - Cost of Goods Sold
+          CAST(ROUND(COALESCE(total_sold, 0) - (COALESCE(number_of_goods_sold, 0) * COALESCE(avg_price_of_ending_inventory, 0)), 2) AS DECIMAL(15,2)) AS gross_profit
         FROM product p
-        LEFT JOIN (SELECT
-                    COUNT(product_id) AS times_stocked, 
-                    SUM(quantity) AS total_stock,
-                    AVG(unit_price) AS average_price,
-                    SUM(quantity * unit_price) AS total_price,
+
+        -- query unused stock from previous date
+        LEFT JOIN (SELECT 
+                      COALESCE(s.purchases, 0) - COALESCE(ol.number_of_goods_sold, 0) AS ending_inventory,
+                      s.avg_price_of_purchases AS avg_price_of_ending_inventory,
+                      p.product_id
+                  FROM product p
+                  LEFT JOIN (SELECT 
+                                SUM(quantity) AS purchases,
+                                AVG(unit_price) AS avg_price_of_purchases,
+                              product_id
+                              FROM stock
+                              WHERE date_supplied 
+                                BETWEEN DATE_FORMAT(:startDate - INTERVAL 1 MONTH, '%Y-%m-01')
+                                AND DATE_FORMAT(LAST_DAY(:endDate - INTERVAL 1 MONTH), '%Y-%m-%d')
+                              GROUP BY product_id) s ON s.product_id = p.product_id
+                  LEFT JOIN (SELECT
+                                SUM(ol.quantity) AS number_of_goods_sold,
+                                ol.product_id
+                              FROM order_line ol
+                              INNER JOIN ${"`order`"} o ON o.order_id = ol.order_id
+                              WHERE o.date_placed 
+                                BETWEEN DATE_FORMAT(:startDate - INTERVAL 1 MONTH, '%Y-%m-01')
+                                AND DATE_FORMAT(LAST_DAY(:endDate - INTERVAL 1 MONTH), '%Y-%m-%d')
+                              GROUP BY ol.product_id) ol on ol.product_id = p.product_id) prev_inv ON prev_inv.product_id = p.product_id
+        
+        -- query purchases / stocks from stock
+        LEFT JOIN (SELECT 
+                    SUM(quantity) AS purchases,
+                    AVG(unit_price) AS avg_price_of_purchases,
                     product_id
                   FROM stock
                   WHERE date_supplied BETWEEN :startDate AND :endDate
-                  GROUP BY product_id) s ON s.product_id = p.product_id
-        LEFT JOIN order_line ol ON p.product_id = ol.product_id
-        GROUP BY p.product_id
-        ORDER BY gross_profit DESC, times_stocked DESC, product_name ASC`,
+                  GROUP BY product_id) s ON s.product_id = p.product_id 
+        
+        -- query orders
+        LEFT JOIN (SELECT
+                    SUM(ol.quantity) AS number_of_goods_sold,
+                    AVG(ol.selling_price) AS avg_price_of_goods_sold,
+                    SUM(ol.selling_price * ol.quantity) AS total_sold,
+                    ol.product_id
+                  FROM order_line ol
+                  INNER JOIN ${"`order`"} o ON o.order_id = ol.order_id
+                  WHERE o.date_placed BETWEEN :startDate AND :endDate
+                  GROUP BY ol.product_id) ol on ol.product_id = p.product_id
+        ORDER BY gross_profit DESC, product_name ASC`,
         {
           startDate: startDate || Report.defaults.START_DATE,
           endDate: endDate || Report.defaults.END_DATE,
@@ -104,27 +198,13 @@ class Report {
       );
 
       await conn.end();
-      content = data;
+      retVal = data;
     } catch (err) {
       console.log("[INVENTORY REPORT]", err.message);
       throw new InternalServerError(err);
     }
 
-    return [
-      {
-        sheet: "Inventory",
-        columns: [
-          { label: "Product	Name", value: "product_name" },
-          { label: "Number of Times Stocked", value: "times_stocked" },
-          { label: "Quantity Used", value: "quantity_used", format: '###,###,###.# "items"' },
-          { label: "Quantity Unused", value: "quantity_unused", format: '###,###,###.# "items"' },
-          { label: "Cost of Goods Sold", value: "cost_of_goods_sold", format: "₱###,###,###.00" },
-          { label: "Gross Profit", value: "gross_profit", format: "₱###,###,###.00" },
-          { label: "Net Profit", value: "net_profit", format: "₱###,###,###.00" },
-        ],
-        content,
-      },
-    ];
+    return retVal;
   }
 
   static async retrieveProductPerformance({ startDate, endDate, limit, desc = false }) {
